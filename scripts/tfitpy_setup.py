8de8f1d93830bd92a4a41b9a412284203f7e9f4f
shubh <shubhvjain@gmail.com>
2026-03-15 13:30:20 +0100

    setup file

--- scripts/tfitpy_setup.py
+++ scripts/tfitpy_setup.py
@@ -2,8 +2,11 @@
 """
 
 from tfitpy.datasets.setup import install
+from tfitpy.datasets.pair_cache import build,load
+
 import os
 
 if __name__ == "__main__":
     data_path = os.getenv("DATA_PATH")
     install(data_path)
+    build(data_path,n_jobs=-1, batch_size=200000)
